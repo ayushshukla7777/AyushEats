b4c1461fbed486966a2939c22336cbd86c52d6f8
Ayush Aryan <ayushshukla7777@gmail.com>
2024-03-02 00:07:46 +0530

    Fixed Router, Auth and env

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,19 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './global.css'
-import { BrowserRouter as Router } from "react-router-dom";
-import AppRoutes from './AppRoutes'
+import { RouterProvider } from "react-router-dom";
+
 import AuthenticationProvider from './auth/AuthenticationProvider'
+import router from './NewAppRoutes';
+
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Router>
-      <AuthenticationProvider>
-        <AppRoutes/>
-      </AuthenticationProvider>
-    </Router>
-
-
+    <AuthenticationProvider>
+      <RouterProvider router={router}></RouterProvider>
+    </AuthenticationProvider>
   </React.StrictMode>,
 )
+
+
+// import AppRoutes from './AppRoutes'
+// import { BrowserRouter as Route } from 'react-router-dom'
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+//   <React.StrictMode>
+//     <Router>
+//       <AuthenticationProvider>
+//         <AppRoutes/>
+//       </AuthenticationProvider>
+//     </Router>
+
+
+//   </React.StrictMode>,
+// )
